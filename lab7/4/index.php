1ee20aa3b084d4cdd1c04016a3db98b90bac9ad2
kisaragikz <87943175+kisaragikz@users.noreply.github.com>
2022-10-03 14:38:44 +0700

    Add files via upload

---
+++ lab7/4/index.php
@@ -0,0 +1,61 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Tech Lab7</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+    
+        body {
+            background-color: hsl(214, 17%, 8%);
+            display: grid;
+            grid-template-columns: 24% 24% 24% 24%;
+            grid-gap: 1%;
+        }
+    
+        img {
+            width: 100%;
+            border-radius: 10px;
+            margin: 2%;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "
+    <div>
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393645433245737/483.jpeg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393645680701500/482.jpeg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393646272086077/481.jpeg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393646595063868/478.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394094240546857/367.png\">
+    </div>
+    <div>
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393646825746452/477.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393647085781042/473.png\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393647391985704/470.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393647647825970/469.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394091786870824/427.jpg\">
+    </div>
+    <div>
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393647916257300/454.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026393648285372447/453.png\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394090830581770/458.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394091149336656/435.png\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394091480690738/435.jpg\">
+    </div>
+    <div>
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394092038537266/426.png\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394092491513886/419.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394092768346112/407.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394093070340096/378.jpg\">
+        <img src=\"https://cdn.discordapp.com/attachments/882338565378437170/1026394093577830421/375.jpg\">
+    </div>
+    "
+    ?>
+</body>
+</html>
