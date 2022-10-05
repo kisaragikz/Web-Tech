833faebdc90b8854c5bd21155bf0149f27afabb1
kisaragikz <87943175+kisaragikz@users.noreply.github.com>
2022-10-05 12:33:24 +0700

    Update index.php

--- lab8/2/index.php
+++ lab8/2/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>lab8/2</title>
+    <title>Web Tech Lab8</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
     integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
